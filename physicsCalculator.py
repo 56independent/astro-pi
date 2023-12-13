723ab29f45eded6157122d71ea3eeee96386ef05
mmanel1 <153719977+mmanel1@users.noreply.github.com>
2023-12-13 13:26:24 +0100

    Update physicsCalculator.py

--- physicsCalculator.py
+++ physicsCalculator.py
@@ -1,1 +1,30 @@
-# findSpeed(distance, time, height=ISS_HEIGHT) -> speed (m/s) - Finds our current speed using data; ISS_HEIGHT is a global variable.
+#findSpeed(distance, time, height=ISS_HEIGHT) -> speed (m/s) - Finds our current speed using data; ISS_HEIGHT is a global variable.
+
+pixelDistance = 100
+time = 0.25
+height = 408
+
+def findSpeed(pixelDistance, time, height):
+  import math
+
+  #Determine the line of sight distance from the observer to the ISS using the Pythagorean theorem
+  lineSight = (height**2)+(pixelDistance**2)
+  lineSight = math.sqrt(lineSight)
+
+  #Calculate the angle of elevation
+  angleElevation = pixelDistance / height
+
+  #Use the tangent to find the real-world corresponding distance
+  realDistance = angleElevation * lineSight
+
+  #Calculate the pixel scale factor
+  scaleFactor = realDistance / pixelDistance
+
+  #Adjust for the pixel scale factor
+  finalDistance = realDistance * scaleFactor
+  
+  #Calculate the speed of the ISS
+  speed = finalDistance / time
+  return speed
+
+print(findSpeed(pixelDistance, time, height))
